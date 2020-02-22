c47f8c09756efc98b672d696e26874c32efe1596
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-22 18:59:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,24 +8,29 @@ def map(source_array)
   array
 end
 
-def reduce(source_array, starting_point = nil)
-  if starting_point
-    i = 0
-    num1 = starting_point
-  while i < source_array.length do
-    num1 += source_array[i]
-    i += 1
-  end
-  num1
-  else
-  i = 0
-  while i < source_array.length do
-    if yield(source_array[i]) == false
-      return false
-    end
-      i += 1
-  end
-  return true
-end
-end
+
+
+
+
+
+# def reduce(source_array, starting_point = nil)
+#   if starting_point
+#     i = 0
+#     num1 = starting_point
+#   while i < source_array.length do
+#     num1 += source_array[i]
+#     i += 1
+#   end
+#   num1
+#   else
+#   i = 0
+#   while i < source_array.length do
+#     if yield(source_array[i]) == false
+#       return false
+#     end
+#       i += 1
+#   end
+#   return true
+# end
+# end
 # returns a running total when not given a starting point
