c7899293bce07204029c6bd9a0ebdb63809d2530
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-22 19:29:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,16 +9,16 @@ def map(source_array)
 end
 
 def reduce(source_array, starting_point = nil)
-#   if starting_point
-#     i = 0
-#     num1 = starting_point
-#   else
-#     num1 = source_array[0]
-#     i = 1
-#   end
-#   while i < source_array.length do
-#     num1 = yield(num1, source_array[i])
-#       i += 1
-#   end
-#   return num1
+  if starting_point
+    i = 0
+    num1 = starting_point
+  else
+    num1 = source_array[0]
+    i = 1
+  end
+  while i < source_array.length do
+    num1 = yield(num1, source_array[i])
+      i += 1
+  end
+  return num1
 end
