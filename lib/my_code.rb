95df723e57c9b1430a86261afe1750ae8f980765
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-22 18:05:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(source_array)
+  array = []
+  i = 0
+  while i < source_array.length
+    array << yield(source_array[i])
+    i += 1
+  end
+  array
+end
+
+def reduce(source_array, starting_value = nil) 
+  if starting_value
+  starting_value = 0
+  else
+  i = 0
+  while i < source_array.length do
+    if yield(source_array[i]) == false
+      return false
+    end
+      i += 1
+  end
+  end
+  return true
+end
