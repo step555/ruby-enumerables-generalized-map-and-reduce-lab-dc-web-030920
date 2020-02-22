916996f4ac2afc6801974bc2711377a030d93ea3
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-22 19:10:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,35 +17,8 @@ def reduce(source_array, starting_point = nil)
     i = 1
   end
   while i < source_array.length do
-    if yield(source_array[i]) == false
-      return false
-    end
+    num1 = yield(num1, source_array[i])
       i += 1
   end
-  return true
+  return num1
 end
-
-
-
-
-# def reduce(source_array, starting_point = nil)
-#   if starting_point
-#     i = 0
-#     num1 = starting_point
-#   while i < source_array.length do
-#     num1 += source_array[i]
-#     i += 1
-#   end
-#   num1
-#   else
-#   i = 0
-#   while i < source_array.length do
-#     if yield(source_array[i]) == false
-#       return false
-#     end
-#       i += 1
-#   end
-#   return true
-# end
-# end
-# returns a running total when not given a starting point
